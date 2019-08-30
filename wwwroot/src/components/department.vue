0f3ebc4757049523b029b661238751b2ad560d4f
wangyang <18518317472@163.com>
2019-08-30 16:46:48 +0800

    0

--- wwwroot/src/components/department.vue
+++ wwwroot/src/components/department.vue
@@ -10,23 +10,16 @@
       </a-menu>
     </a-layout-sider>
     <a-layout-content>
-      <a-input-search placeholder="search department" />
-      <a-tree
-        :defaultExpandedKeys="expandedKeys"
-        draggable
-        @dragenter="onDragEnter"
-        @drop="onDrop"
-        :treeData="gData"
-      >
-        <template slot="title" slot-scope="{title}">
-          <span v-if="title.indexOf(searchDepartmentValue) > -1">
-            {{title.substr(0, title.indexOf(searchDepartmentValue))}}
-            <span
-              style="color: #f50"
-            >{{searchDepartmentValue}}</span>
-            {{title.substr(title.indexOf(searchDepartmentValue) + searchDepartmentValue.length)}}
+      <a-input-search placeholder="search department" style="width:50%" />
+      <a-button type="default" icon="plus" @click="addTopDept" title="添加顶层部门"></a-button>
+      <a-tree :defaultExpandedKeys="expandedKeys" draggable @drop="onDrop" :treeData="gData">
+        <template slot="custom" slot-scope="item">
+          <span>{{ item.title }}</span>
+          <span class="tree_action_wrap">
+            <a-icon type="plus" class="tree_action" @click.prevent="addDept" />
+            <a-icon type="edit" class="tree_action" @click.prevent="editDept" />
+            <a-icon type="close" class="tree_action" @click.prevent="delDept" />
           </span>
-          <span v-else>{{title}}</span>
         </template>
       </a-tree>
     </a-layout-content>
@@ -38,26 +31,40 @@ const gData = [
   {
     key: 100,
     title: "01",
+    scopedSlots: { title: "custom" },
     children: [
-      { key: 101, title: "01-01" },
-      { key: 102, title: "01-02" },
-      { key: 103, title: "01-03" }
+      { key: 101, title: "01-01", scopedSlots: { title: "custom" } },
+      { key: 102, title: "01-02", scopedSlots: { title: "custom" } },
+      { key: 103, title: "01-03", scopedSlots: { title: "custom" } }
     ]
   },
   {
     key: 200,
     title: "02",
-    children: [{ key: 201, title: "02-01" }, { key: 202, title: "02-02" }]
+    scopedSlots: { title: "custom" },
+    children: [
+      { key: 201, title: "02-01", scopedSlots: { title: "custom" } },
+      ,
+      { key: 202, title: "02-02", scopedSlots: { title: "custom" } }
+    ]
   },
   {
     key: 300,
     title: "03",
-    children: [{ key: 301, title: "03-01" }, { key: 302, title: "03-02" }]
+    scopedSlots: { title: "custom" },
+    children: [
+      { key: 301, title: "03-01", scopedSlots: { title: "custom" } },
+      { key: 302, title: "03-02", scopedSlots: { title: "custom" } }
+    ]
   },
   {
     key: 4,
     title: "04",
-    children: [{ key: 400, title: "04-01" }, { key: 402, title: "04-02" }]
+    scopedSlots: { title: "custom" },
+    children: [
+      { key: 400, title: "04-01", scopedSlots: { title: "custom" } },
+      { key: 402, title: "04-02", scopedSlots: { title: "custom" } }
+    ]
   }
 ];
 
@@ -67,11 +74,16 @@ export default {
       gData,
       searchCompanyValue: "",
       searchDepartmentValue: "04-01",
+      showTools: false,
       expandedKeys: []
     };
   },
   methods: {
     onDragEnter() {},
+    addTopDept() {},
+    addDept() {},
+    editDept() {},
+    delDept() {},
     onDrop(info) {
       //放的位置的key
       const dropKey = info.node.eventKey;
@@ -151,7 +163,21 @@ export default {
   border: 1px solid #e8e8e8;
   background-color: #fff;
 }
-.ant-tree-treenode-selected {
-  background-color: #ccc !important;
+.ant-layout-content {
+  overflow: hidden;
+}
+.ant-tree-node-content-wrapper:hover .tree_action_wrap,
+.ant-tree-treenode-selected .tree_action_wrap {
+  display: inline-block;
+}
+.tree_action_wrap {
+  display: none;
+  margin-left: 10px;
+}
+.tree_action {
+  margin-left: 10px;
+}
+.tree_action:hover {
+  color: red;
 }
 </style>
