9d683758ed13c2704f4d425ce0883c72c51e990e
wangyang <18518317472@163.com>
2020-04-28 09:57:42 +0800

    addsso-mobile

--- wwwroot/vue.config.js
+++ wwwroot/vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
     pages: {
         index: {
             // page 的入口
-            entry: 'src/login.js',
+            entry: 'src/main.js',
             // 模板来源
             template: 'public/index.html',
             // 在 dist/index.html 的输出
