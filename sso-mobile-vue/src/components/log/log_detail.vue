ee7887daa001f32760235ff7327a1ee96ee5e728
wangyang <18518317472@163.com>
2020-06-16 08:41:43 +0800

    addlog

---
+++ sso-mobile-vue/src/components/log/log_detail.vue
@@ -0,0 +1,134 @@
+<template>
+  <f7-page name="log_detail" ptr @ptr:refresh="getData" :infinite-preloader="loading">
+    <f7-navbar title="日志详情" back-link="返回"></f7-navbar>
+    <div class="list no-hairlines-md" v-if="log">
+      <ul>
+        <li class="item-content item-input item-input-with-info">
+          <div class="item-inner">
+            <div class="item-input-wrap">
+              <input type="text" :value="log.From" />
+              <span class="input-clear-button"></span>
+              <div class="item-input-info">来源</div>
+            </div>
+          </div>
+        </li>
+        <li class="item-content item-input item-input-with-info">
+          <div class="item-inner">
+            <div class="item-input-wrap">
+              <input type="text" :value="log.Controller" />
+              <span class="input-clear-button"></span>
+              <div class="item-input-info">Controller</div>
+            </div>
+          </div>
+        </li>
+        <li class="item-content item-input item-input-with-info">
+          <div class="item-inner">
+            <div class="item-input-wrap">
+              <input type="text" :value="log.Action" />
+              <span class="input-clear-button"></span>
+              <div class="item-input-info">Action</div>
+            </div>
+          </div>
+        </li>
+        <li class="item-content item-input item-input-with-info">
+          <div class="item-inner">
+            <div class="item-input-wrap">
+              <input type="text" :value="log.Route||''" />
+              <span class="input-clear-button"></span>
+              <div class="item-input-info">Route</div>
+            </div>
+          </div>
+        </li>
+        <li class="item-content item-input item-input-with-info">
+          <div class="item-inner">
+            <div class="item-input-wrap">
+              <input type="text" :value="log.QueryString" />
+              <span class="input-clear-button"></span>
+              <div class="item-input-info">查询</div>
+            </div>
+          </div>
+        </li>
+        <li class="item-content item-input item-input-with-info">
+          <div class="item-inner">
+            <div class="item-input-wrap">
+              <input type="text" :value="log.Content" />
+              <span class="input-clear-button"></span>
+              <div class="item-input-info">内容</div>
+            </div>
+          </div>
+        </li>
+        <li class="item-content item-input item-input-with-info">
+          <div class="item-inner">
+            <div class="item-input-wrap">
+              <input type="text" :value="log.UserId" />
+              <span class="input-clear-button"></span>
+              <div class="item-input-info">用户id</div>
+            </div>
+          </div>
+        </li>
+        <li class="item-content item-input item-input-with-info">
+          <div class="item-inner">
+            <div class="item-input-wrap">
+              <input type="text" :value="log.UserName" />
+              <span class="input-clear-button"></span>
+              <div class="item-input-info">用户名称</div>
+            </div>
+          </div>
+        </li>
+        <li class="item-content item-input item-input-with-info">
+          <div class="item-inner">
+            <div class="item-input-wrap">
+              <input type="text" :value="log.UserHost" />
+              <span class="input-clear-button"></span>
+              <div class="item-input-info">用户代理</div>
+            </div>
+          </div>
+        </li>
+        <li class="item-content item-input item-input-with-info">
+          <div class="item-inner">
+            <div class="item-input-wrap">
+              <input type="text" :value="log.CreateTime" />
+              <span class="input-clear-button"></span>
+              <div class="item-input-info">创建时间</div>
+            </div>
+          </div>
+        </li>
+        <li class="item-content item-input item-input-with-info"></li>
+      </ul>
+    </div>
+    <f7-block class="text-align-center" v-else>
+      <f7-preloader></f7-preloader>
+    </f7-block>
+  </f7-page>
+</template>
+
+<script>
+export default {
+  name: "log_detail",
+  data() {
+    return {
+      log: null,
+      loading: false
+    };
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    getData(done) {
+      this.loading = true;
+      var id = this.$f7route.params.id;
+      this.$axios.get(this.$urls.log.detail + "/" + id).then(response => {
+        this.loading = false;
+        if (done) done();
+        if (response.code === 0) {
+          this.log = response.result;
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ sso-mobile-vue/src/components/log/log_manage.vue
@@ -0,0 +1,184 @@
+<template>
+  <f7-page
+    name="log_manage"
+    ptr
+    @ptr:refresh="refresh"
+    infinite
+    :infinite-distance="50"
+    :infinite-preloader="loading"
+    @infinite="loadMore"
+  >
+    <f7-navbar title="日志列表" back-link="返回">
+      <f7-nav-right>
+        <f7-link icon-f7="hourglass_tophalf_fill" sheet-open=".sheet-top"></f7-link>
+      </f7-nav-right>
+    </f7-navbar>
+    <f7-sheet top backdrop class="sheet-top">
+      <f7-toolbar bottom>
+        <div class="left"></div>
+        <div class="right">
+          <f7-link sheet-close @click="onSearch">确定</f7-link>
+        </div>
+      </f7-toolbar>
+      <f7-page-content>
+        <f7-block-title>来源</f7-block-title>
+        <f7-block strong>
+          <f7-chip
+            :color="item.from==fromcheck?'blue':''"
+            :text="item.from"
+            v-for="item in froms"
+            :key="item.from"
+            @click="fromClick"
+          ></f7-chip>
+        </f7-block>
+        <f7-block-title>Controller</f7-block-title>
+        <f7-block strong>
+          <f7-chip
+            :color="item.controller==controllercheck?'blue':''"
+            :text="item.controller"
+            v-for="item in controllers"
+            :key="item.controller"
+            @click="controllerClick"
+          ></f7-chip>
+        </f7-block>
+        <f7-block-title>Action</f7-block-title>
+        <f7-block strong>
+          <f7-chip
+            :color="item.action==actioncheck?'blue':''"
+            :text="item.action"
+            v-for="item in actions"
+            :key="item.action"
+            @click="actionClick"
+          ></f7-chip>
+        </f7-block>
+        <f7-block-title>时间</f7-block-title>
+        <f7-block strong>
+          <f7-chip :color="day==30?'blue':''" text="1个月内" data-day="30" @click="timeClick"></f7-chip>
+          <f7-chip :color="day==90?'blue':''" text="3个月内" data-day="90" @click="timeClick"></f7-chip>
+          <f7-chip :color="day==180?'blue':''" text="6个月内" data-day="180" @click="timeClick"></f7-chip>
+          <f7-chip :color="day==360?'blue':''" text="1年之内" data-day="360" @click="timeClick"></f7-chip>
+        </f7-block>
+      </f7-page-content>
+    </f7-sheet>
+    <f7-searchbar disable-button-text placeholder="Search" :clear-button="true" @change="onSearch"></f7-searchbar>
+    <f7-list media-list>
+      <f7-list-item
+        v-for="item in datas"
+        :title="item.Controller+'/'+item.Action"
+        :subtitle="item.CreateTime"
+        :key="item._id"
+        :after="item.UserName"
+        :link="'/logdetail/'+item._id"
+      ></f7-list-item>
+    </f7-list>
+    <f7-block class="text-align-center" v-if="datas.length===0&&isEnd">没有数据</f7-block>
+    <f7-block class="text-align-center" v-if="datas.length>0&&isEnd">---end---</f7-block>
+  </f7-page>
+</template>
+
+<script>
+import ListBase from "../ListBase";
+export default {
+  name: "log_manage",
+  mixins: [ListBase],
+  data() {
+    return {
+      getlist: this.$urls.log.getlistsimple,
+      fromcheck: "",
+      froms: [],
+      controllercheck: "",
+      controllers: [],
+      actioncheck: "",
+      actions: [],
+      day: 30
+    };
+  },
+  created() {
+    this.getFroms();
+  },
+  methods: {
+    getQuerystring() {
+      var url = "?pageIndex=" + this.pageIndex + "&pageSize=" + this.pageSize;
+      if (this.fromcheck) url += "&from=" + this.fromcheck;
+      if (this.controllercheck)
+        url += "&controllerName=" + this.controllercheck;
+      if (this.actioncheck) url += "&actionName=" + this.actioncheck;
+      if (this.filter) url += "&userName=" + this.filter;
+      if (this.day)
+        url +=
+          "&startTime=" +
+          this.$funtools.dateAddDays(new Date(), -this.day, "yyyy-MM-dd");
+      return url;
+    },
+    getFroms() {
+      this.$axios.get(this.$urls.log.getfromlist).then(response => {
+        if (response.code === 0) this.froms = response.result;
+      });
+    },
+    getControllers(from) {
+      this.$axios
+        .get(this.$urls.log.getControllersByFrom + "?from=" + from)
+        .then(response => {
+          if (response.code === 0) this.controllers = response.result;
+        });
+    },
+    getActions(from, controller) {
+      this.$axios
+        .get(
+          this.$urls.log.getActionsByController +
+            "?from=" +
+            from +
+            "&controllerName=" +
+            controller
+        )
+        .then(response => {
+          if (response.code === 0) this.actions = response.result;
+        });
+    },
+    fromClick($event) {
+      var from = $event.currentTarget.innerText;
+      if (this.fromcheck == from) {
+        this.fromcheck = "";
+        return;
+      }
+      this.fromcheck = from;
+      this.actions = [];
+      this.getControllers(from);
+    },
+    controllerClick($event) {
+      var controller = $event.currentTarget.innerText;
+      if (this.controllercheck == controller) {
+        this.controllercheck = "";
+        return;
+      }
+      this.controllercheck = controller;
+      this.actions = [];
+      this.getActions(this.fromcheck, controller);
+    },
+    actionClick($event) {
+      var action = $event.currentTarget.innerText;
+      if (this.actioncheck == action) {
+        this.actioncheck = "";
+        return;
+      }
+      this.actioncheck = action;
+    },
+    timeClick($event) {
+      var day = $event.currentTarget.dataset.day;
+      this.day = day;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.block-title {
+  margin-top: 5px;
+}
+.sheet-modal {
+  min-height: 50%;
+}
+.chip {
+  margin-left: 5px;
+}
+</style>
