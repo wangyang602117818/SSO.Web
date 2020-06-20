88c492aea26ca1437044a6737678c9b85fcc7ec2
wangyang <18518317472@163.com>
2020-06-20 10:43:32 +0800

    update

--- sso-mobile-vue/src/components/navigator.vue
+++ sso-mobile-vue/src/components/navigator.vue
@@ -5,13 +5,21 @@
     </div>
     <div class="nav_content">
       <div class="sub_title">导航列表</div>
-      <f7-row v-for="(item,index) in datas" :key="index" no-gap>
-        <f7-col v-for="(it,i) in item" :key="i">
-          <div v-if="it" class="nav_wrap" :data-url="it.BaseUrl" @click="itemClick">
-            <img class="nav_logo" :src="it.LogoUrl" alt />
-            <div class="nav_title">
-              <span>{{it.Title}}</span>
-            </div>
+      <f7-row no-gap>
+        <f7-col>
+          <div class="nav_wrap" @click="itemClick">
+            <img class="nav_logo"  alt />
+            <div class="nav_title"><span>文件管理</span></div>
+          </div>
+        </f7-col>
+        <f7-col>
+          <div class="nav_wrap">
+            
+          </div>
+        </f7-col>
+        <f7-col>
+          <div class="nav_wrap">
+           
           </div>
         </f7-col>
       </f7-row>
@@ -24,15 +32,11 @@ export default {
   name: "navigator",
   data() {
     return {
-      datas: []
+      
     };
   },
   created() {
-    this.$axios.get(this.$urls.navigation.geturlmeta).then(response => {
-      if (response.code === 0) {
-        this.datas = this.$funtools.reMapArray(response.result, 3);
-      }
-    });
+    
   },
   methods: {
     itemClick($event) {
