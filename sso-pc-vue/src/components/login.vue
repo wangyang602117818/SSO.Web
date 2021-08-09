417f8977c52e91658a04b35e1617a48161e88ce4
Yang Xa Wang <60211781@qq.com>
2021-08-09 10:47:04 +0800

    bug

--- sso-pc-vue/src/components/login.vue
+++ sso-pc-vue/src/components/login.vue
@@ -4,19 +4,34 @@
       <div class="login_title">登录</div>
       <div class="login_content">
         <div class="input_wrap">
-          <div :class="userErrorLine?'input_content error':'input_content'">
+          <div :class="userErrorLine ? 'input_content error' : 'input_content'">
             <svg viewBox="64 64 896 896" width="14" height="14" fill="#aaaaaa">
               <path
                 d="M858.5 763.6a374 374 0 0 0-80.6-119.5 375.63 375.63 0 0 0-119.5-80.6c-.4-.2-.8-.3-1.2-.5C719.5 518 760 444.7 760 362c0-137-111-248-248-248S264 225 264 362c0 82.7 40.5 156 102.8 201.1-.4.2-.8.3-1.2.5-44.8 18.9-85 46-119.5 80.6a375.63 375.63 0 0 0-80.6 119.5A371.7 371.7 0 0 0 136 901.8a8 8 0 0 0 8 8.2h60c4.4 0 7.9-3.5 8-7.8 2-77.2 33-149.5 87.8-204.3 56.7-56.7 132-87.9 212.2-87.9s155.5 31.2 212.2 87.9C779 752.7 810 825 812 902.2c.1 4.4 3.6 7.8 8 7.8h60a8 8 0 0 0 8-8.2c-1-47.8-10.9-94.3-29.5-138.2zM512 534c-45.9 0-89.1-17.9-121.6-50.4S340 407.9 340 362c0-45.9 17.9-89.1 50.4-121.6S466.1 190 512 190s89.1 17.9 121.6 50.4S684 316.1 684 362c0 45.9-17.9 89.1-50.4 121.6S557.9 534 512 534z"
               />
             </svg>
-            <input type="text" id="userId" placeholder="用户编号" v-model="userId" @input="userChange" />
+            <input
+              type="text"
+              id="userId"
+              placeholder="用户编号"
+              v-model="userId"
+              @input="userChange"
+            />
           </div>
-          <div class="input_error">{{userIdExists?'':'用户编号是必填项'}}</div>
+          <div class="input_error">
+            {{ userIdExists ? "" : "用户编号是必填项" }}
+          </div>
         </div>
         <div class="input_wrap">
-          <div :class="passWordErrorLine?'input_content error':'input_content'">
-            <svg viewBox="64 64 896 896" width="14px" height="14px" fill="#aaaaaa">
+          <div
+            :class="passWordErrorLine ? 'input_content error' : 'input_content'"
+          >
+            <svg
+              viewBox="64 64 896 896"
+              width="14px"
+              height="14px"
+              fill="#aaaaaa"
+            >
               <path
                 d="M832 464h-68V240c0-70.7-57.3-128-128-128H388c-70.7 0-128 57.3-128 128v224h-68c-17.7 0-32 14.3-32 32v384c0 17.7 14.3 32 32 32h640c17.7 0 32-14.3 32-32V496c0-17.7-14.3-32-32-32zM332 240c0-30.9 25.1-56 56-56h248c30.9 0 56 25.1 56 56v224H332V240zm460 600H232V536h560v304zM484 701v53c0 4.4 3.6 8 8 8h40c4.4 0 8-3.6 8-8v-53a48.01 48.01 0 1 0-56 0z"
               />
@@ -29,14 +44,16 @@
               @input="passWordChange"
             />
           </div>
-          <div class="input_error">{{passWordExists?'':'用户密码是必填项'}}</div>
+          <div class="input_error">
+            {{ passWordExists ? "" : "用户密码是必填项" }}
+          </div>
         </div>
       </div>
       <div class="login_button">
         <input
           type="button"
           class="submit_button"
-          :disabled="loading==true"
+          :disabled="loading == true"
           value="登 录"
           @click="handleLogin"
         />
@@ -44,7 +61,13 @@
     </div>
     <transition name="move">
       <div class="toast" v-show="mssage_show">
-        <svg viewBox="0 0 1024 1024" version="1.1" p-id="2179" width="18" height="18">
+        <svg
+          viewBox="0 0 1024 1024"
+          version="1.1"
+          p-id="2179"
+          width="18"
+          height="18"
+        >
           <path
             d="M512 85.333333a426.666667 426.666667 0 1 0 426.666667 426.666667A426.666667 426.666667 0 0 0 512 85.333333z m42.666667 597.333334a42.666667 42.666667 0 0 1-85.333334 0v-213.333334a42.666667 42.666667 0 0 1 85.333334 0z m-42.666667-298.666667a42.666667 42.666667 0 1 1 42.666667-42.666667 42.666667 42.666667 0 0 1-42.666667 42.666667z"
             fill="#eb8c00"
@@ -67,12 +90,10 @@ export default {
       passWordExists: true,
       userErrorLine: false,
       passWordErrorLine: false,
-      mssage_show: false
+      mssage_show: false,
     };
   },
-  mounted() {
-    
-  },
+  mounted() {},
   methods: {
     userChange(e) {
       var value = e.currentTarget.value;
@@ -92,27 +113,33 @@ export default {
           .post(this.$urls.login, {
             userId: this.userId,
             passWord: this.passWord,
-            returnUrl: returnUrl
+            returnUrl: returnUrl,
           })
-          .then(response => {
+          .then((response) => {
             if (response.code == 0) {
               var returnUrl = response.result;
               window.location.href = decodeURIComponent(returnUrl);
             } else {
               this.mssage_show = true;
-              setTimeout(()=>{this.mssage_show=false},2000);
+              setTimeout(() => {
+                this.mssage_show = false;
+              }, 2000);
               this.loading = false;
             }
           });
       } else {
-        this.userIdExists=false;
-        this.passWordExists=false;
-        this.userErrorLine=true;
-        this.passWordErrorLine=true;
+        if (this.userId.trim().length == 0) {
+          this.userIdExists = false;
+          this.userErrorLine = true;
+        }
+        if (this.passWord.trim().length == 0) {
+          this.passWordExists = false;
+          this.passWordErrorLine = true;
+        }
         this.loading = false;
       }
-    }
-  }
+    },
+  },
 };
 </script>
 <style scoped>
@@ -237,7 +264,7 @@ body {
   -webkit-box-shadow: 0 2px 0 rgba(0, 0, 0, 0.045);
   box-shadow: 0 2px 0 rgba(0, 0, 0, 0.045);
   cursor: pointer;
-  height: 32px;
+  height: 36px;
   padding: 0 15px;
   width: 100%;
   outline: 0;
