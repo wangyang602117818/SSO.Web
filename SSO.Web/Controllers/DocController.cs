96d191413112c25530af6fbce243046ab5dc2b7a
Yang Xa Wang <60211781@qq.com>
2022-10-12 17:34:14 +0800

    更新文档

--- SSO.Web/Controllers/DocController.cs
+++ SSO.Web/Controllers/DocController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 namespace SSO.Web.Controllers
 {
     [AllowAnonymous]
+    [NoneLogRecord]
     public class DocController : Controller
     {
         public ActionResult Index(string id)
